914af8d39b7f6329edba11e17ae0ff6dd06879c2
Maicon Heck <13810070+maiconheck@users.noreply.github.com>
2020-03-15 10:17:49 +0000

    :recycle: analyzers updated

--- src/Krafted.IntegrationTest/GlobalSuppressions.cs
+++ src/Krafted.IntegrationTest/GlobalSuppressions.cs
@@ -3,5 +3,8 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CC0057:Unused parameters", Justification = "This parameter is necessary for the extension method.", Scope = "member", Target = "~M:Krafted.IntegrationTest.Migration.KraftedDataBase.UseMigration(Microsoft.AspNetCore.Builder.IApplicationBuilder,System.String)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1175:Unused this parameter.", Justification = "Needed for extension method", Scope = "member", Target = "~M:Krafted.IntegrationTest.Migration.KraftedDataBase.UseMigration(Microsoft.AspNetCore.Builder.IApplicationBuilder,System.String)")]
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CC0057:Unused parameters", Justification = "This parameter is necessary for the extension method.", Scope = "member", Target = "~M:Krafted.IntegrationTest.Migration.KraftedDataBase.UseMigration(Microsoft.AspNetCore.Builder.IApplicationBuilder,System.String)")]
+[assembly: SuppressMessage("Redundancy", "RCS1175:Unused this parameter.", Justification = "Needed for extension method", Scope = "member", Target = "~M:Krafted.IntegrationTest.Migration.KraftedDataBase.UseMigration(Microsoft.AspNetCore.Builder.IApplicationBuilder,System.String)")]
+[assembly: SuppressMessage("Usage", "CA1806:Do not ignore method results", Justification = "Just to test", Scope = "member", Target = "~M:Krafted.IntegrationTest.Data.Connection.SqlServer.SqlServerConnectionProviderTest.NewInstance_FilledConnectionString_DoesNotThrowsException(System.String)")]
