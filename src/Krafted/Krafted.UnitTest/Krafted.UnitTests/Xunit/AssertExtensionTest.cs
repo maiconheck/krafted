9b00bb6e0e3fead792401e2f824b6ce69fc504ee
Maicon Heck <13810070+maiconheck@users.noreply.github.com>
2023-09-08 09:22:14 -0300

    feature(new AssertExtension methods): Verifies that a type contains (or not) `Null Guard Clauses` for all parameters of it constructor. - Assert.ContainsNullGuardClause<T>(...) - Assert.DoesNotContainNullGuardClause<T>(...)

--- src/Krafted/Krafted.UnitTest/Krafted.UnitTests/Xunit/AssertExtensionTest.cs
+++ src/Krafted/Krafted.UnitTest/Krafted.UnitTests/Xunit/AssertExtensionTest.cs
@@ -16,5 +16,38 @@ namespace Krafted.UnitTest.Krafted.UnitTests.Xunit
                 Guard.Against.Null(param);
             });
         }
+
+        [Fact]
+        public void ContainsNullGuardClause_ModelWithNullGuardClausesForAllParameters_Success()
+        {
+            string param1 = "My parameter 1";
+            string param2 = "My parameter 2";
+            string param3 = "My parameter 3";
+            string param4 = "My parameter 4";
+            string param5 = "My parameter 5";
+            Assert.ContainsNullGuardClause<ModelWithNullGuardClausesForAllParameters>(param1, param2, param3, param4, param5);
+        }
+
+        [Fact]
+        public void DoesNotContainNullGuardClause_ModelWithNullGuardClausesForSomeParameters_Fail()
+        {
+            string param1 = "My parameter 1";
+            string param2 = "My parameter 2";
+            string param3 = "My parameter 3";
+            string param4 = "My parameter 4";
+            string param5 = "My parameter 5";
+            Assert.DoesNotContainNullGuardClause<ModelWithNullGuardClausesForSomeParameters>(param1, param2, param3, param4, param5);
+        }
+
+        [Fact]
+        public void DoesNotContainNullGuardClause_ModelWithoutNullGuardClauses_Fail()
+        {
+            string param1 = "My parameter 1";
+            string param2 = "My parameter 2";
+            string param3 = "My parameter 3";
+            string param4 = "My parameter 4";
+            string param5 = "My parameter 5";
+            Assert.DoesNotContainNullGuardClause<ModelWithoutNullGuardClauses>(param1, param2, param3, param4, param5);
+        }
     }
 }
